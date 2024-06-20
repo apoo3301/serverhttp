6706208bbcd701dfc2364748d7d62fdfd6fc95d5
witchbl4des <menzo.smit-adam@epitech.eu>
2024-06-21 01:59:42 +0200

    linked list setup + header parse

--- lockedroom/src/networking/protocol/httpRequest.h
+++ lockedroom/src/networking/protocol/httpRequest.h
@@ -2,6 +2,8 @@
 #define httpRequest_h
 
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "../../datastructures/dictionary/dictionary.h"
 
 enum httpMethod {
@@ -20,6 +22,7 @@ struct httpRequest {
 	int Method;
 	char* Path;
 	float httpVersion;
+	struct Dictionary header_fields;
 };
 
 struct httpRequest http_request_constructor(char *request_string);
